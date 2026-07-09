e619ae7f9314de77365b1d03f743b5a9978648fc
jessymcdermott <jmcdermott2014@gmail.com>
2026-07-09 09:30:58 -0400

    pls pls pls send scan to gmq

--- src/secrets/hardcoded_secret.py
+++ src/secrets/hardcoded_secret.py
@@ -1,1 +1,3 @@
 password = 'ThisIsBadPractice123!'  # Hardcoded password, should be avoided
+awsKey = 'AKIAIOSFODNN7EXAMPLE'  # Hardcoded AWS key, should be avoided
+awsSecret = 'FAKEwJalrXUtnFEMI/K7MDENG/bPxRfiCY4kHXAP4Th7'  # Hardcoded AWS secret, should be avoided
