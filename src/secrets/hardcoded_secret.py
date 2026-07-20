5fb94a52b4912b205f46b451c037b98d1fe6fe95
jessymcdermott <44265711+jessymcdermott@users.noreply.github.com>
2026-07-20 17:32:29 -0400

    Update hardcoded password in hardcoded_secret.py
    Replaced hardcoded password with a new value while retaining the hardcoded AWS key and secret.

--- src/secrets/hardcoded_secret.py
+++ src/secrets/hardcoded_secret.py
@@ -1,3 +1,5 @@
 password = 'ThisIsBadPractice123!'  # Hardcoded password, should be avoided
 awsKey = 'AKIAIOSFODNN7EXAMPLE'  # Hardcoded AWS key, should be avoided
 awsSecret = 'FAKEwJalrXUtnFEMI/K7MDENG/bPxRfiCY4kHXAP4Th7'  # Hardcoded AWS secret, should be avoided
+
+password = 'barkbark'
